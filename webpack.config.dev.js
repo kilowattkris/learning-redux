fb038fe90e780505ecd47f55ade1d0062ec7b8a4
Kristine Tomlinson <ktomlinson@elltechnologies.com>
2017-05-02 11:25:18 -0400

    added files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -39,7 +39,7 @@ export default {
           includePaths: [path.resolve(__dirname, 'src', 'scss')]
         },
         context: '/',
-        postcss: () => [autoprefixer],
+        postcss: () => [autoprefixer]
       }
     })
   ],
